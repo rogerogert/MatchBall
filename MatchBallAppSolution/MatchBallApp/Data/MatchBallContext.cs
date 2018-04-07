231e0266819ac8e3922ce287487d1f76b4022707
Rogerio Teixeira <rogerioteixeira91@gmail.com>
2018-04-07 18:55:16 -0300

    EF Core instalado e criada classe contexto

---
+++ MatchBallAppSolution/MatchBallApp/Data/MatchBallContext.cs
@@ -0,0 +1,16 @@
+﻿using MatchBallApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchBallApp.Data
+{
+    public class MatchBallContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
--- MatchBallAppSolution/MatchBallApp/Startup.cs
+++ MatchBallAppSolution/MatchBallApp/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MatchBallApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MatchBallApp
@@ -15,6 +17,10 @@ namespace MatchBallApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MatchBallContext>(cfg => 
+            {
+                cfg.UseSqlServer("");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
